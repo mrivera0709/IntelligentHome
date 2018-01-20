601928858d29ee003ef8f0aad9246aa7d1799aa5
Misael Rivera <mrivera0709@gmail.com>
2018-01-20 11:55:44 -0500

    Css update and materialize elements added

--- intelligenthome/resources/views/home.blade.php
+++ intelligenthome/resources/views/home.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-2">
-            
-
-
+<div class="container ">
+    <div class="row ">
+        <div class="col-md-3 col-md-2">
+                
+        </div>
+        <div class="col-md-9 col-md-2">
+                
         </div>
     </div>
 </div>
--- intelligenthome/resources/views/layouts/app.blade.php
+++ intelligenthome/resources/views/layouts/app.blade.php
@@ -17,39 +17,55 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="black">
 <header>
     <div id="app">
 
         <nav class="black">
             <div class="nav-wrapper container">
-              <a href="{{ url('/home') }}" class="brand-logo"> {{ config('app.name', 'Laravel') }} </a>
-              <ul id="nav-mobile" class="right hide-on-med-and-down links">
-                <li> <a href="{{ route('about') }}">ABOUT</a> </li>
-                <li> <a href="{{ route('privacy') }}">PRIVACY</a> </li>
-                <li> <a href="{{ route('affiliate') }}">AFFILIATE PROGRAM</a> </li>
-              </ul>
+                <a href="{{ url('/home') }}" class="brand-logo textGradient1 font36"> {{ config('app.name', 'Laravel') }} </a>
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+
+                <ul id="nav-mobile" class="right hide-on-med-and-down links">
+                    <li class="textGradient2"> <a class="font16" href="{{ route('about') }}">ABOUT</a> </li>
+                    <li class="textGradient2"> <a class="font16" href="{{ route('privacy') }}">PRIVACY</a> </li>
+                    <li class="textGradient2"> <a class="font16" href="{{ route('affiliate') }}">AFFILIATE PROGRAM</a> </li>
+                </ul>
+
+                <ul class="side-nav" id="mobile-demo">
+                    <li class="textGradient2"> <a class="font16" href="{{ route('about') }}">ABOUT</a> </li>
+                    <li class="textGradient2"> <a class="font16" href="{{ route('privacy') }}">PRIVACY</a> </li>
+                    <li class="textGradient2"> <a class="font16" href="{{ route('affiliate') }}">AFFILIATE PROGRAM</a> </li>
+                </ul>
+
             </div>
           </nav>
 </header>
 
 <main>
         @yield('content')
+        
     </div>
 </main>
 
 <footer class="page-footer black">
     <div class="footer-copyright">
-        <div class="container">
-        © 2018 COPYRIGHT MISAEL RIVERA
+        <div class="container textGradient2 bold">
+        © 2018 COPYRIGHT | MISAEL RIVERA
         </div>
-     </div>
+    </div>
 </footer>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script type="text/javascript">$( document ).ready(function(){
+        $(".button-collapse").sideNav();
+    })</script>
+    
+    
 </body>
 </html>
